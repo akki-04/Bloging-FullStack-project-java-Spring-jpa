96e65dc587c19f9d127884099af4e1af8fd1dd73
akki-04 <akash16yadav2004@gmail.com>
2025-12-15 16:57:47 +0530

    Delete Bloggingproject directory

--- Bloggingproject/src/main/java/com/blog/Bloggingproject/repository/postRepo.java
+++
@@ -1,7 +0,0 @@
-package com.blog.Bloggingproject.repository;
-import com.blog.Bloggingproject.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface postRepo extends JpaRepository<Post,Integer> {
-    
-}
